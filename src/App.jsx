d78192917810527d428d74d6f0d92ec8f1f78e58
Josue Romay <joxromaylp@gmail.com>
2023-06-23 12:00:32 -0400

    Componentes

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import About from './componentes/About'
+import DashBoard from './componentes/DashBoard'
+import Home from './componentes/Home'
+
+function App() {
+  return(
+    <div>
+      <Header/>
+      {/* <Home/> */}
+      {/* <About/> */}
+      {/* <DashBoard/> */}
+    </div>
+  )
+}
+
+export default App
